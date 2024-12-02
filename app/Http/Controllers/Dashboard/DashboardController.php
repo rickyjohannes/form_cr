e50726dacf7e035afe27ebd27e1cd915d5b9908a
rickyjohannes <rickyjop0@gmail.com>
2024-12-02 16:15:02 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app/Http/Controllers/Dashboard/DashboardController.php

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -38,10 +38,17 @@ class DashboardController extends Controller
         ->where('proposals.status_apr', 'fully_approved')
         ->groupBy('users.name', 'proposals.status_barang')
         ->get();
+        $countJeninsPermintaanByIT = Proposal::select('it_user', 'status_barang', DB::raw('COUNT(*) as count'))
+        ->where('status_apr', 'fully_approved')
+        ->whereNotNull('it_user') // Menambahkan kondisi untuk memastikan it_user tidak null
+        ->groupBy('it_user', 'status_barang')
+        ->get();
         $ratingByUserIT = Proposal::select('it_user', DB::raw('AVG(rating_it) as rating'))
         ->whereIn('status_cr', ['Closed', 'Auto Close'])
         ->groupBy('it_user')
         ->get();
+
+    //   /  dd($countJeninsPermintaanByIT);
 
         $proposalCount = Proposal::count();
         $pending = Proposal::where('status_apr', 'pending')->count();
@@ -95,6 +102,7 @@ class DashboardController extends Controller
             'crCounts' => $crCounts,  // Add the CR counts to the data
             'countJeninsPermintaan' => $countJeninsPermintaan,  // Add this line
             'countJeninsPermintaanByUser' => $countJeninsPermintaanByUser,  // Add this line
+            'countJeninsPermintaanByIT' => $countJeninsPermintaanByIT,
             'ratingByUserIT' => $ratingByUserIT 
         ];
 
@@ -114,10 +122,17 @@ class DashboardController extends Controller
         ->where('proposals.status_apr', 'fully_approved')
         ->groupBy('users.name', 'proposals.status_barang')
         ->get();
+        $countJeninsPermintaanByIT = Proposal::select('it_user', 'status_barang', DB::raw('COUNT(*) as count'))
+        ->where('status_apr', 'fully_approved')
+        ->whereNotNull('it_user') // Menambahkan kondisi untuk memastikan it_user tidak null
+        ->groupBy('it_user', 'status_barang')
+        ->get();
         $ratingByUserIT = Proposal::select('it_user', DB::raw('AVG(rating_it) as rating'))
         ->whereIn('status_cr', ['Closed', 'Auto Close'])
         ->groupBy('it_user')
         ->get();
+
+    //   /  dd($countJeninsPermintaanByIT);
 
         $proposalCount = Proposal::count();
         $pending = Proposal::where('status_apr', 'pending')->count();
@@ -171,6 +186,7 @@ class DashboardController extends Controller
             'crCounts' => $crCounts,  // Add the CR counts to the data
             'countJeninsPermintaan' => $countJeninsPermintaan,  // Add this line
             'countJeninsPermintaanByUser' => $countJeninsPermintaanByUser,  // Add this line
+            'countJeninsPermintaanByIT' => $countJeninsPermintaanByIT,
             'ratingByUserIT' => $ratingByUserIT 
         ];
 
@@ -272,10 +288,17 @@ class DashboardController extends Controller
        ->where('proposals.status_apr', 'fully_approved')
        ->groupBy('users.name', 'proposals.status_barang')
        ->get();
+       $countJeninsPermintaanByIT = Proposal::select('it_user', 'status_barang', DB::raw('COUNT(*) as count'))
+       ->where('status_apr', 'fully_approved')
+       ->whereNotNull('it_user') // Menambahkan kondisi untuk memastikan it_user tidak null
+       ->groupBy('it_user', 'status_barang')
+       ->get();
        $ratingByUserIT = Proposal::select('it_user', DB::raw('AVG(rating_it) as rating'))
        ->whereIn('status_cr', ['Closed', 'Auto Close'])
        ->groupBy('it_user')
        ->get();
+
+   //   /  dd($countJeninsPermintaanByIT);
 
        $proposalCount = Proposal::count();
        $pending = Proposal::where('status_apr', 'pending')->count();
@@ -329,6 +352,7 @@ class DashboardController extends Controller
            'crCounts' => $crCounts,  // Add the CR counts to the data
            'countJeninsPermintaan' => $countJeninsPermintaan,  // Add this line
            'countJeninsPermintaanByUser' => $countJeninsPermintaanByUser,  // Add this line
+           'countJeninsPermintaanByIT' => $countJeninsPermintaanByIT,
            'ratingByUserIT' => $ratingByUserIT 
        ];
 
