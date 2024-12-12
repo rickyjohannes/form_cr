95a7b4b788f8b430223e4a29ee29495294ac7394
rickyjohannes <rickyjop0@gmail.com>
2024-12-12 10:43:22 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   resources/views/dashboard/proposal/it.blade.php
    	modified:   resources/views/dashboard/proposal/user.blade.php

--- resources/views/dashboard/proposal/it.blade.php
+++ resources/views/dashboard/proposal/it.blade.php
@@ -39,7 +39,7 @@
                         <label for="status_cr" class="font-weight-bold text-left">&#x1F50D; Filter Status CR:</label>
                         <select id="status_cr" name="status_cr" class="form-control" style="max-width: 250px;">
                             <option value="">Select Status CR</option>
-                            <option value="OPEN">OPEN</option>
+                            <option value="OPEN">Open</option>
                             <option value="Open To IT">Open To IT</option>
                             <option value="ON PROGRESS">On Progress</option>
                             <option value="DELAY">Delay</option>
